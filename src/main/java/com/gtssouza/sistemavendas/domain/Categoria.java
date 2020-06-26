9aebce209c4345bb9c62af3568732ee92748cbc1
Gustavo Souza <gtssouza94@gmail.com>
2020-06-26 20:06:43 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/gtssouza/sistemavendas/domain/Categoria.java
+++ src/main/java/com/gtssouza/sistemavendas/domain/Categoria.java
@@ -2,9 +2,18 @@ package com.gtssouza.sistemavendas.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
-	private static final long serialVersionUID = 6502004946577537778L;
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
